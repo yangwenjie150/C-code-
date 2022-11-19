5a0cd2bcf85d874e74fd7d5fd841d60105edc925
yangwenjie150 <118618887+yangwenjie150@users.noreply.github.com>
2022-11-20 00:09:23 +0800

    Create m-nhe.c
    #include<stdio.h>
    int main()
    {
        int m,n,i,sum=0;
        printf("请输入两个整数m和n(要求:m<n):");
        scanf("%d,%d",&m,&n);
        for(i=m; i<=n; i++)
           {
            sum=sum+i;
    	   }
            printf("%d和%d之间所有的整数和是：%d",m,n,sum);  
        return 0;
    }

---
+++ m-nhe.c
@@ -0,0 +1,7 @@
+echo "# C-code-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yangwenjie150/C-code-.git
+git push -u origin main
